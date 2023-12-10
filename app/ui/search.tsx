72255c97c175a3ff8379e4e9254ba925e8fa3cf0
nakayamaryo0731 <ryo_nakayama@caddi.jp>
2023-12-10 15:17:56 +0900

    fix: fix some

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,8 +4,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-export default function Search() {
-  const placeholder="Search invoices..."
+export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -13,7 +12,7 @@ export default function Search() {
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
-
+    
     if (term) {
       params.set('query', term);
     } else {
